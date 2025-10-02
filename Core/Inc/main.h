b3d3fe1ac45b2fe7b1634da49c09a2ab0f6fcec6
zbxboris <luobian1140@sjtu.edu.cn>
2025-10-02 16:10:26 +0800

    feat: switch to HSE

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -57,14 +57,6 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
-#define LEDR_Pin GPIO_PIN_12
-#define LEDR_GPIO_Port GPIOH
-#define LEDG_Pin GPIO_PIN_11
-#define LEDG_GPIO_Port GPIOH
-#define LEDB_Pin GPIO_PIN_10
-#define LEDB_GPIO_Port GPIOH
-#define KEY_Pin GPIO_PIN_0
-#define KEY_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
