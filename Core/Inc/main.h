d07fe390cfb78e4b157c3ecad6d44741d1235fa3
zbxboris <luobian1140@sjtu.edu.cn>
2025-10-02 16:43:23 +0800

    feat: iwdg watched 1Hz breathing led

--- Core/Inc/main.h
+++ Core/Inc/main.h
@@ -61,6 +61,8 @@ void Error_Handler(void);
 #define PWMR_GPIO_Port GPIOH
 #define LEDG_Pin GPIO_PIN_11
 #define LEDG_GPIO_Port GPIOH
+#define KEY_Pin GPIO_PIN_0
+#define KEY_GPIO_Port GPIOA
 
 /* USER CODE BEGIN Private defines */
 
